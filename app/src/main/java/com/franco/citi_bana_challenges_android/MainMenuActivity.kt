a85c9907cb14db122ba6c845e2c900052a6bfca7
franco <franquicidad@gmail.com>
2021-02-27 14:48:32 -0500

    changed name as suggested.

--- app/src/main/java/com/franco/citi_bana_challenges_android/MainMenuActivity.kt
+++ app/src/main/java/com/franco/citi_bana_challenges_android/MainMenuActivity.kt
@@ -3,7 +3,7 @@ package com.franco.citi_bana_challenges_android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
