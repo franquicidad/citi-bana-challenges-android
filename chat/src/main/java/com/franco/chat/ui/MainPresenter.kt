d802123cff1f41953157464d836b06ac5a52d308
franco <franquicidad@gmail.com>
2021-02-28 19:36:48 -0500

    clean arquitecture almost done missing di and mvp 2

--- chat/src/main/java/com/franco/chat/ui/MainPresenter.kt
+++ chat/src/main/java/com/franco/chat/ui/MainPresenter.kt
@@ -1,8 +1,9 @@
 package com.franco.chat.ui
 
 import com.franco.usecases.InterfaceGetAllUsersUseCase
+import javax.inject.Inject
 
-class MainPresenter(
+class MainPresenter @Inject constructor(
     private val getAllUsersUseCase: InterfaceGetAllUsersUseCase
 ) {
 interface View{
--- data/src/main/java/com/franco/data/UserRepository.kt
+++ data/src/main/java/com/franco/data/UserRepository.kt
@@ -3,36 +3,13 @@ package com.franco.data
 import com.franco.DANIELA
 import com.franco.RODRIGO
 import com.franco.domain.User
+import javax.inject.Inject
 
-class UserRepository (
+class UserRepository @Inject constructor(
+    private val remoteDataSourceSim: RemoteDataSourceSim
     ):RemoteDataSourceSim {
     override fun createAllUsers(): List<User> {
-        val list = mutableListOf<User>()
-        with(list) {
-            add(User(1, RODRIGO, "Hola como esta?", "9:01 am"))
-            add(User(2, DANIELA, "Aqui trabajando", "9:14 am"))
-            add(User(1, RODRIGO, "Y eso has conseguido un empleo?", "9:15 am"))
-            add(
-                User(
-                    2,
-                    DANIELA,
-                    "Si he conseguido como desarrolladora android en la mejor empresa del mundo sus valores y trabajo en equipo son geniales",
-                    "9:30 am"
-                )
-            )
-            add(User(1, RODRIGO, "Y tu crees que me puedas a ayudar a entrar?", "9:31 am"))
-            add(
-                User(
-                    2,
-                    DANIELA,
-                    "Claro que si!!! incluso me daran un bono si entras !!! pasame tu Cv",
-                    "10:00 am"
-                )
-            )
-            add(User(1, RODRIGO, "Gracias!!!!", "10:00 am"))
-
-        }
-        return list
+        return remoteDataSourceSim.createAllUsers()
     }
 }
 
