ffb3a3b1a6dd0d54d8ef87ee2faa8b086f46c01e
franco <franquicidad@gmail.com>
2021-03-08 09:25:22 -0500

    added animation

---
+++ animate/src/main/java/com/franco/animate/Animation.kt
@@ -0,0 +1,11 @@
+package com.franco.animate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Animation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animation)
+    }
+}
